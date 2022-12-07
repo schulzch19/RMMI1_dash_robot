d71bc9e00a19b109c64b8466932ef43cad13f158
schulzch19 <118679417+schulzch19@users.noreply.github.com>
2022-12-08 00:10:34 +0100

    joints update corrected

--- dash_robot.py
+++ dash_robot.py
@@ -1,5 +1,4 @@
 from dash import Dash, dcc, html, Input, Output, ALL, State, MATCH, ALLSMALLER, ctx
-from dash_extensions.enrich import Output, DashProxy, Input, MultiplexerTransform, State
 import plotly.express as px
 import plotly.graph_objects as go
 from visual_kinematics.RobotSerial import *
@@ -7,15 +6,13 @@ from math import pi
 import numpy as np
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-np.set_printoptions(precision=3, suppress=True)
+np.set_printoptions(precision=5, suppress=True)
 
-# app = Dash(__name__, external_stylesheets=external_stylesheets)
-# app = Dash(__name__)
-app = DashProxy(prevent_initial_callbacks=False, transforms=[MultiplexerTransform()], external_stylesheets=external_stylesheets)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-robot = None
 
+# draw arrows
 def drawArrow(start, end, color, w, cone_width):
     print("draw arrow")
     x = [start[0], end[0]]
@@ -42,6 +39,7 @@ def drawArrow(start, end, color, w, cone_width):
         )
     ]
 
+# draw orientations
 def drawMat(mat, lw, scale, cone):
     print("draw mat")
     arrow_x = drawArrow([mat[0, 3], mat[1, 3], mat[2, 3]], [mat[0, 3]+mat[0, 0]*scale, mat[1, 3]+mat[1, 0]*scale, mat[2, 3]+mat[2, 0]*scale], "red", lw, cone)
@@ -50,6 +48,7 @@ def drawMat(mat, lw, scale, cone):
     xy = arrow_x + arrow_y
     return xy + arrow_z
 
+# draw the robot
 def draw_robot(robot, height, w_link, w_joint, w_arrow, l_arrow, c_arrow):
     print("draw robot")
     poses = [[0, 0, 0]]
@@ -84,7 +83,6 @@ app.layout = html.Div([
     html.H1("Model your own robot using DH parameters"),
     html.H2("Settings"),
     html.Div([
-        html.Div(["number of joints: ", dcc.Input(id='nbr_joints', value='6', type='number', style={'width': '5em'})], style={"fontWeight": "bold"}), html.Br(),
         " fig height: ",
         dcc.Input(id='fig_height', value='500', type='number', style={'width': '5em'}), html.Br(),
         " link width: ", 
@@ -100,6 +98,7 @@ app.layout = html.Div([
 
     ]),
     html.H2("DH parameters"),
+    html.Div(["number of joints: ", dcc.Input(id='nbr_joints', value='6', type='number', style={'width': '5em'})], style={"fontWeight": "bold"}), 
     html.Table([], id='joints_table'),
     html.Br(),
     html.Div([
@@ -113,13 +112,13 @@ app.layout = html.Div([
     ], style={'marginBottom': "1rem"}),
     html.Div([
         "Rotation: ",
-        dcc.Input(id='rot-i', value=0, type='number', style={'width': '5em'}),
+        dcc.Input(id='rot-i', value=0, type='number', style={'width': '7em'}),
         " i + ",
-        dcc.Input(id='rot-j', value=0, type='number', style={'width': '5em'}),
+        dcc.Input(id='rot-j', value=0, type='number', style={'width': '7em'}),
         " j + ",
-        dcc.Input(id='rot-k', value=0, type='number', style={'width': '5em'}),
+        dcc.Input(id='rot-k', value=0, type='number', style={'width': '7em'}),
         " k + ",
-        dcc.Input(id='rot-w', value=0, type='number', style={'width': '5em'}),
+        dcc.Input(id='rot-w', value=0, type='number', style={'width': '7em'}),
     ]),
     html.Br(),
     html.Div([
@@ -135,28 +134,7 @@ app.layout = html.Div([
 
 ], style={'marginLeft': "1rem", 'fontSize': 20})
 
-
-# change robot parameters
-# @app.callback(
-#     Output(component_id='test-input', component_property='value'),
-#     State(component_id='joints_table', component_property='children'),
-#     Input(component_id={"name": ALL, "type": "dyn-in"}, component_property='value')
-# )
-# def update_robot(test_in, val):
-#     print("hey" + str(val))
-#     head = test_in[0]
-#     content = test_in[1]
-#     dh_params = []
-#     print([e['props']['children'] for e in head['props']['children']['props']['children']])
-#     for l in content['props']['children']:
-#         axis = [float(e['props']['children']['props']['value']) for e in l['props']['children'][1:]]
-#         print(axis)
-#         dh_params.append([axis[0], axis[1], axis[2]/180*pi, axis[3]/180*pi])
-#     print(dh_params)
-#     robot = RobotSerial(dh_params)
-
-#     return 5
-
+# update graph and printed matrices
 @app.callback(
     Output('graph-robot', 'figure'),
     Output('pos-x', 'value'),
@@ -178,19 +156,18 @@ app.layout = html.Div([
 )
 def update_robot(params, angles, h, w_link, w_joint, w_arrow, l_arrow, c_arrow):
     print("update robot")
-    global robot
     triggered_id = ctx.triggered_id
-    try:
-        if triggered_id['type'] == "dyn-in":
-            dh_params = []
-            for l in range(0, int(len(params)/4)):
-                axis = [float(n) for n in params[4*l:4*l+4]]
-                dh_params.append([axis[0], axis[1], axis[2]/180*pi, axis[3]/180*pi])
-            dh_params = np.array(dh_params)
-            print(dh_params)
-            robot = RobotSerial(dh_params)
-    except:
-        pass
+    # try:
+    #     if triggered_id['type'] == "dyn-in":
+    dh_params = []
+    for l in range(0, int(len(params)/4)):
+        axis = [float(n) for n in params[4*l:4*l+4]]
+        dh_params.append([axis[0], axis[1], axis[2]/180*pi, axis[3]/180*pi])
+    dh_params = np.array(dh_params)
+    print(dh_params)
+    robot = RobotSerial(dh_params)
+    # except:
+    #     pass
     robot_angles = np.array([float(a)/180*pi for a in angles])
     f = robot.forward(robot_angles)
     
@@ -225,59 +202,12 @@ def update_robot(params, angles, h, w_link, w_joint, w_arrow, l_arrow, c_arrow):
                     ]) for i in range(1,len(matrices))]
         )]
 
-# @app.callback(
-#     Output('graph-robot', 'figure'),
-#     Output('pos-x', 'value'),
-#     Output('pos-y', 'value'),
-#     Output('pos-z', 'value'),
-#     Output('rot-i', 'value'),
-#     Output('rot-j', 'value'),
-#     Output('rot-k', 'value'),
-#     Output('rot-w', 'value'),
-#     Output('mats_table', 'children'),
-#     Input(component_id={"name": ALL, "type": "dyn-in-angle"}, component_property='value'),
-#     Input(component_id='fig_height', component_property='value'),
-#     Input(component_id='width_link', component_property='value'),
-#     Input(component_id='width_joint', component_property='value'),
-#     Input(component_id='width_arrow', component_property='value'),
-#     Input(component_id='length_arrow', component_property='value'),
-#     Input(component_id='size_cone_arrow', component_property='value')
-# )
-# def update_robot_angles(angles, h, w_link, w_joint, w_arrow, l_arrow, c_arrow):
-#     print("update robot angles")
-#     global robot
-#     robot_angles = np.array([float(a)/180*pi for a in angles])
-#     f = robot.forward(robot_angles)
-
-#     fig, matrices = draw_robot(robot, h, w_link, w_joint, w_arrow, l_arrow, c_arrow)
-
-
-
-#     # fig.update_layout(transition_duration=500)
-#     pos = f.t_3_1.reshape([3, ])
-#     quat = f.q_4
-
-#     return fig, \
-#         "{:.6f}".format(pos[0]), "{:.6f}".format(pos[1]), "{:.6f}".format(pos[2]), \
-#         "{:.6f}".format(quat[0]), "{:.6f}".format(quat[1]), "{:.6f}".format(quat[2]), "{:.6f}".format(quat[3]), \
-#         [html.Thead(
-#             html.Tr([html.Th("joint"), html.Th("Matrix")])
-#         ),
-#         html.Tbody([
-#             html.Tr([
-#                     html.Th(i), 
-#                     html.Td(html.Div([
-#                         "{}".format(matrices[i][0]), html.Br(), 
-#                         "{}".format(matrices[i][1]), html.Br(),
-#                         "{}".format(matrices[i][2]), html.Br(),
-#                         "{}".format(matrices[i][3])
-#                         ]))
-#                     ]) for i in range(1,len(matrices))]
-#         )]
-
+# set angles to 0 or calculate the inverse kinematics
 @app.callback(
+    Output(component_id={"name": ALL, "type": "dyn-in-angle"}, component_property='value'),
     Input('inverse-btn', 'n_clicks'),
     Input('angle-reset-btn', 'n_clicks'),
+    State(component_id={"name": ALL, "type": "dyn-in"}, component_property='value'),
     State('pos-x', 'value'),
     State('pos-y', 'value'),
     State('pos-z', 'value'),
@@ -285,54 +215,66 @@ def update_robot(params, angles, h, w_link, w_joint, w_arrow, l_arrow, c_arrow):
     State('rot-j', 'value'),
     State('rot-k', 'value'),
     State('rot-w', 'value'),
-    Output(component_id={"name": ALL, "type": "dyn-in-angle"}, component_property='value'),
+    State(component_id={"name": ALL, "type": "dyn-in-angle"}, component_property='value'),
 )
-def calc_inverse(n_c_inv, n_c_res, x, y, z, i, j, k, w):
+def calc_inverse(n_c_inv, n_c_res, params, x, y, z, i, j, k, w, angles):
+    print("calc inverse")
     triggered_id = ctx.triggered_id
-    global robot
-
-    if triggered_id == 'inverse-btn':
-        print("calc inverse")
-
-        xyz = np.array([[x], [y], [z]])
-        quat = np.array([i, j, k, w])
-        end = Frame.from_q_4(quat, xyz)
-        robot.inverse(end)
-        angles = robot.axis_values
-
-        return tuple(["{:.3f}".format(a/pi*180) for a in angles])
-    else:
-        print("reset angles")
-
-        angles = [0 for i in range(robot.num_axis)]
-
-        return tuple(["{:.3f}".format(a/pi*180) for a in angles])
-
-
-@app.callback(
-    # State(component_id={"name": ALL, "type": "dyn-in"}, component_property='value'),
-    # State('nbr_joints', 'value'),
-    Input('dh-reset-btn', 'n_clicks'),
-    Output(component_id={"name": ALL, "type": "dyn-in"}, component_property='value'),
-)
-def reset_dh(n_c):
-    print("\n\n\n\n\n\nWHYYYYY???????\n\n\n\n\n\n")
-    triggered_id = ctx.triggered_id
-    global robot
     print(triggered_id)
-    return [0 for _ in range(robot.num_axis*4)]
+    if triggered_id == None:
+        print("none detected")
+        return angles
+    else:
+        dh_params = []
+        for l in range(0, int(len(params)/4)):
+            axis = [float(n) for n in params[4*l:4*l+4]]
+            dh_params.append([axis[0], axis[1], axis[2]/180*pi, axis[3]/180*pi])
+        dh_params = np.array(dh_params)
+        print(dh_params)
+        robot = RobotSerial(dh_params)
 
-# @app.callback(
-#     Input('angle-reset-btn', 'n_clicks'),
-#     Output(component_id={"name": ALL, "type": "dyn-in-angle"}, component_property='value'),
-# )
-# def reset_angles(n_c):
-#     print("reset angles")
-#     global robot
+        if triggered_id == 'inverse-btn':
 
-#     angles = [0 for i in range(robot.num_axis)]
+            xyz = np.array([[x], [y], [z]])
+            quat = np.array([i, j, k, w])
+            end = Frame.from_q_4(quat, xyz)
+            robot.inverse(end)
+            angles = robot.axis_values
 
-#     return tuple(["{:.3f}".format(a/pi*180) for a in angles])
+            return tuple(["{:.3f}".format(a/pi*180) for a in angles])
+        elif triggered_id == 'angle-reset-btn':
+            print("reset angles")
+
+            angles = [0 for i in range(robot.num_axis)]
+
+            return tuple(["{:.3f}".format(a/pi*180) for a in angles])
+        
+        else:
+            print('\n\n\n\n\n\nWHY calc inverse ??????????\n\n\n\n\n\n')
+            angles = [5 for i in range(robot.num_axis)]
+
+            return tuple(["{:.3f}".format(a/pi*180) for a in angles])
+
+# reset dh parameters
+@app.callback(
+    Output(component_id={"name": ALL, "type": "dyn-in"}, component_property='value'),
+    Input('dh-reset-btn', 'n_clicks'),
+    State('nbr_joints', 'value'),
+    State(component_id={"name": ALL, "type": "dyn-in"}, component_property='value'),
+)
+def reset_dh(n_c, nbr_joints, params):
+    print('reset_dh')
+    triggered_id = ctx.triggered_id
+    print(triggered_id)
+    if triggered_id == None:
+        print("none detected")
+        return params
+    else:
+        if triggered_id == 'dh-reset-btn':
+            return [0 for _ in range(int(nbr_joints)*4)]
+        else:
+            print('\n\n\n\n\n\nWHY reset dh ??????????\n\n\n\n\n\n')
+            return [12 for _ in range(int(nbr_joints)*4)]
 
 # change number of joints
 @app.callback(
@@ -363,22 +305,6 @@ def update_output_div(joints, params, angles):
         ]),
 
     ], max(1, int(joints))
-
-
-# # change figure height
-# @app.callback(
-#     Output('graph-robot', 'figure'),
-#     State('graph-robot', 'figure'),
-#     Input(component_id='fig_height', component_property='value'),
-#     Input(component_id='width_link', component_property='value'),
-#     Input(component_id='width_joint', component_property='value'),
-#     Input(component_id='width_arrow', component_property='value'),
-#     Input(component_id='length_arrow', component_property='value')
-#     )
-# def update_figure(fig_old, h):
-#     print("update figure")
-#     fig = go.Figure(fig_old)
-#     return fig.update_layout(height = max(10, int(h)))
 
 if __name__ == '__main__':
     app.run_server(debug=True)
